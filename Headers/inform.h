c198e2f75f64c9119f18e058ba17b25a950ec151
Den <dennykypanchukfrl@gmail.com>
2021-03-28 17:11:22 +0300

    Data entry implemented

---
+++ Headers/inform.h
@@ -0,0 +1,11 @@
+#ifndef PATTERNWORD_INFORM_H
+#define PATTERNWORD_INFORM_H
+
+//path to demo file
+// /Home/Documents/Template.txt
+//Output file name - pattern_word.txt
+
+//main.c
+void FreeMemory(char *);
+
+#endif //PATTERNWORD_INFORM_H
--- main.c
+++ main.c
@@ -1,6 +1,32 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "Headers/inform.h"
 
-int main( )
+#define SIZE 200
+
+int main(int argc, char* argv[])
 {
+    //Pointer to the path to file
+    char *PathToFile = NULL;
+    if (argc > 1)
+    {
+        printf("File is transferred to the program in following way:\nPath => %s\n", argv[2]);
+        PathToFile = strdup(argv[2]);
+    }
+    else
+    {
+        char tempSent[SIZE];
+        printf("None file are founded. Please write full path to your file:\n");
+        gets(tempSent);
+        PathToFile = strdup(tempSent);
+    }
+    printf("Entered path => %s", PathToFile);
+    FreeMemory(PathToFile);
     return 0;
+}
+//Delete dynamic array
+void FreeMemory(char *Path)
+{
+    free(Path);
 }
